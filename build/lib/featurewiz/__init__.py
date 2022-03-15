d6daa456158751a1e2b9449a071c67b40de0e712
Francisco Parrilla <francisco.parrilla@housesimple.com>
2022-03-15 09:44:39 +0000

    zzz

--- build/lib/featurewiz/__init__.py
+++ build/lib/featurewiz/__init__.py
@@ -33,6 +33,8 @@ if __name__ == "__main__":
 else:
     module_type = 'Imported'
 version_number = __version__
+
+print("THIS IS A CUSTOM VERSION!!!!")
 print("""%s featurewiz. Version = %s. nrows=None uses all rows. Set nrows=1000 to randomly sample 1000 rows.
 outputs = featurewiz(dataname, target, corr_limit=0.70, verbose=2, sep=',', 
 		header=0, test_data='',feature_engg='', category_encoders='',
